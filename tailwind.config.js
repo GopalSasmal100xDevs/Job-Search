4f3663c26cb0b8770a96df0b080556af2eb1caa7
gopalprojectx100 <gopalsasmal100xdevs@gmail.com>
2024-10-19 10:30:01 +0530

    Navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    fontFamily: {
-      sans: ["Roboto", "sans-serif"],
-    },
-    gridTemplateColumns: {
-      "70/30": "70% 28%",
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+      gridTemplateColumns: {
+        "70/30": "70% 28%",
+      },
     },
   },
   plugins: [],
