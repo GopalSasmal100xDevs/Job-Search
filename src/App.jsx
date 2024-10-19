ab3bc11b8042d4321205444c543ea41ab4b109a0
gopalprojectx100 <gopalsasmal100xdevs@gmail.com>
2024-10-19 11:25:43 +0530

    Job listing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Hero } from "./components/Hero";
+import { HomeCard } from "./components/HomeCards";
+import JobListing from "./components/JobListing";
 import Navbar from "./components/Navbar";
 
 export default function App() {
@@ -6,6 +8,8 @@ export default function App() {
     <>
       <Navbar />
       <Hero />
+      <HomeCard />
+      <JobListing />
     </>
   );
 }
